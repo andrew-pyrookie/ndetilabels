6f2413a474f1c34afb57bd80c4bb2a01c856f6b6
andrew-pyrookie <andrewthepyrookie@gmail.com>
2023-10-05 11:16:26 +0300

    chnages

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("user-icon").addEventListener("click", function(e) {
+    e.preventDefault();
+    var form = document.querySelector(".form");
+    form.style.display = "block";
+});
